987ff1e1249ff7964418459f3c7d86b5dbb42fbd
mark953fps <markville953@gmail.com>
2021-09-17 10:19:39 +0800

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  region = 'MACAU';
+  region = 'MACAU'; //assuming this is the region that you used in the api.
 
   ngOnInit() {
     if (this.region !== 'CYPRUS') {
